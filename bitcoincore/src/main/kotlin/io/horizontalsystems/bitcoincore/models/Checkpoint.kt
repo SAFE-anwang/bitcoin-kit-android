9e2e802624007fa6da548b833f6a8227dc3d81d0
LongWen <416441008@qq.com>
2023-10-16 22:30:45 +0800

    fix liquidity

--- bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/models/Checkpoint.kt
+++ bitcoincore/src/main/kotlin/io/horizontalsystems/bitcoincore/models/Checkpoint.kt
@@ -1,8 +1,6 @@
 package io.horizontalsystems.bitcoincore.models
 
-import android.util.Log
 import io.horizontalsystems.bitcoincore.extensions.hexToByteArray
-import io.horizontalsystems.bitcoincore.extensions.toHexString
 import io.horizontalsystems.bitcoincore.io.BitcoinInputMarkable
 import io.horizontalsystems.bitcoincore.storage.BlockHeader
 import java.io.BufferedReader
@@ -35,7 +33,6 @@ class Checkpoint(val fileName: String) {
             val nonce = input.readUnsignedInt()
             val height = input.readInt()
             val hash = input.readBytes(32)
-            Log.e("Peer[]", "fileName=$fileName,  hash=${hash.toHexString()}, prevHash=${prevHash.toHexString()}, version=$version, timestamp=$timestamp, bits=$bits, nonce=$nonce, height=$height")
             return Block(BlockHeader(
                     version = version,
                     previousBlockHeaderHash = prevHash,
