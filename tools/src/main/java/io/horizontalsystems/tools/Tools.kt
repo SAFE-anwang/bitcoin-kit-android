cb8dc8c4021559beca5551653e1a1d21a7814a73
LongWen <416441008@qq.com>
2023-10-16 22:28:50 +0800

    update safe fallback

--- tools/src/main/java/io/horizontalsystems/tools/Tools.kt
+++ tools/src/main/java/io/horizontalsystems/tools/Tools.kt
@@ -20,24 +20,14 @@ private fun syncCheckpoints() {
 
 private fun buildCustomCheckpoint() {
     val checkpointBlock = Block(BlockHeader(
-            version = 6422788,
-            previousBlockHeaderHash = HashUtils.toBytesAsLE("19c4e740302b8761e2e20b4edb71ff5015b49a53e261ca5a83c3a9227729faa4"),
-            merkleRoot = HashUtils.toBytesAsLE("3568b15113142af64ba4eb5c536aa25da84e993d753797ab5c0bb2fec7dd58bb"),
-            timestamp = 1690179853,
+            version = 536870912,
+            previousBlockHeaderHash = HashUtils.toBytesAsLE("ffb185f7e0ba5c72ae9cc45178575eb7029db5e8cca7f747b532cd0b9ab74a2c"),
+            merkleRoot = HashUtils.toBytesAsLE("17fac79b2c00f3e13740d3e04c0e5f1f028cef65127ba7331479b069a638163c"),
+            timestamp = 1694780205,
             bits = 0,
-            nonce = 0,
-            hash = HashUtils.toBytesAsLE("394932a5df926f71582229bdc533108d4396c056d8291a8c7cf4396c6ac5a74f")
-    ), 5194611)
-
-    /*val checkpointBlock = Block(BlockHeader(
-            version = 6422788,
-            previousBlockHeaderHash = HashUtils.toBytesAsLE("ef26bb216f3720451368d3dbc83d5d9a2ee380bca287c31d4671d00771c93c42"),
-            merkleRoot = HashUtils.toBytesAsLE("e11952dc9499338b0198c47d925d0fb0a9a31318af2c03712afe130be0b4aa0b"),
-            timestamp = 1657646280,
-            bits = 436421877,
-            nonce = 0,
-            hash = HashUtils.toBytesAsLE("ed7d266dcbd8bb8af80f9ccb8deb3e18f9cc3f6972912680feeb37b090f8cee0")
-    ), 4303965)*/
+            nonce = 123806790,
+            hash = HashUtils.toBytesAsLE("3ef061097a1edea33f712d1f99e4be8567e1094a5a01e79f4d196eaad5cd6ab0")
+    ), 5178101)
 
     BuildCheckpoints().build(checkpointBlock)
 }
