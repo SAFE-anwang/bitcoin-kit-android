50fdaa6f351bd9d1cd202aed97745eeda4cc4985
LongWen <416441008@qq.com>
2023-12-21 20:09:05 +0800

    update checkpoint

--- tools/src/main/java/io/horizontalsystems/tools/Tools.kt
+++ tools/src/main/java/io/horizontalsystems/tools/Tools.kt
@@ -21,13 +21,13 @@ private fun syncCheckpoints() {
 private fun buildCustomCheckpoint() {
     val checkpointBlock = Block(BlockHeader(
             version = 536870912,
-            previousBlockHeaderHash = HashUtils.toBytesAsLE("a609e9f3879149e1b3e46a10d59cca5b92ab8c399736b1997e87771a738df7c3"),
-            merkleRoot = HashUtils.toBytesAsLE("b1bfdfa8c592d7d3c6ff6dad3ff3d07bf6b59afa8b0d287053ca514248fd189e"),
-            timestamp = 1701440154,
+            previousBlockHeaderHash = HashUtils.toBytesAsLE("4f6b7c5f4f9b298fa0d6211d17dc252b92e0d5ab45c65c121f1d6c5fd7b82de2"),
+            merkleRoot = HashUtils.toBytesAsLE("87a5518e342bef89de463241f9ab9d1902ba13b9bc3b4078945c53fe1500e8b4"),
+            timestamp = 1702211064,
             bits = 0,
-            nonce = 129738930,
-            hash = HashUtils.toBytesAsLE("3a9ab9b98897756c4ac37919b079b583d5c35e0940c98dc9019ec07f9789d368")
-    ), 5375839)
+            nonce = 129628890,
+            hash = HashUtils.toBytesAsLE("0ca19461c59e31b4d63d21f0b909cf26c46e421d99c01a078a00bc5df472b16a")
+    ), 5400104)
 
     BuildCheckpoints().build(checkpointBlock)
 }
