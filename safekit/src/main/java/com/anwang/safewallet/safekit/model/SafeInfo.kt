8045dece6bc0b3e913b2a5a04943a34fdc948768
Long <416441008@qq.com>
2026-01-30 23:26:27 +0800

    Cross-chain control through interface

--- safekit/src/main/java/com/anwang/safewallet/safekit/model/SafeInfo.kt
+++ safekit/src/main/java/com/anwang/safewallet/safekit/model/SafeInfo.kt
@@ -4,7 +4,7 @@ data class SafeInfo(
     val safe_usdt: String,
     val minamount: String,
     val eth: ChainInfo,
-    val bsc: ChainInfo,
+    val bsc: ChainBscInfo,
     val matic: MaticChainInfo
 ) {
     override fun toString(): String {
@@ -24,6 +24,58 @@ data class ChainInfo(
     }
 }
 
+data class ChainBscInfo(
+    val price: String,
+    val gas_price_gwei: String,
+    val safe_fee: String,
+    val safe2bsc: Boolean,
+    val bsc2safe: Boolean
+) {
+    override fun toString(): String {
+        return "ChainInfo(price='$price', gas_price_gwei='$gas_price_gwei', safe_fee='$safe_fee', safe2bsc=$safe2bsc, eth2safe=$bsc2safe)"
+    }
+}
+
+data class EthUsdtInfo(
+    val price: String,
+    val gas_price_gwei: String,
+    val usdt_fee: String,
+    val safe2eth: Boolean,
+    val eth2safe: Boolean
+) {
+
+}
+
+data class BscUsdtInfo(
+    val price: String,
+    val gas_price_gwei: String,
+    val usdt_fee: String,
+    val safe2bsc: Boolean,
+    val bsc2safe: Boolean
+) {
+
+}
+
+data class SolUsdtInfo(
+    val price: String,
+    val gas_price_gwei: String,
+    val usdt_fee: String,
+    val safe2sol: Boolean,
+    val sol2safe: Boolean
+) {
+
+}
+
+data class TrxUsdtInfo(
+    val price: String,
+    val gas_price_gwei: String,
+    val usdt_fee: String,
+    val safe2trx: Boolean,
+    val trx2safe: Boolean
+) {
+
+}
+
 data class MaticChainInfo(
     val price: String,
     val gas_price_gwei: String,
@@ -35,5 +87,21 @@ data class MaticChainInfo(
         return "MaticChainInfo(price='$price', gas_price_gwei='$gas_price_gwei', safe_fee='$safe_fee', safe2matic=$safe2matic, matic2safe=$matic2safe)"
     }
 }
+
+
+
+data class SafeUsdtInfo(
+    val safe_usdt: String,
+    val minamount: String,
+    val eth: EthUsdtInfo,
+    val bsc: BscUsdtInfo,
+    val sol: SolUsdtInfo,
+    val trx: TrxUsdtInfo
+) {
+    override fun toString(): String {
+        return "SafeInfo(safe_usdt='$safe_usdt', minamount='$minamount', eth=$eth, bsc=$bsc)"
+    }
+}
+
 
 
--- safekit/src/main/java/com/anwang/safewallet/safekit/netwok/SafeProvider.kt
+++ safekit/src/main/java/com/anwang/safewallet/safekit/netwok/SafeProvider.kt
@@ -1,6 +1,7 @@
 package com.anwang.safewallet.safekit.netwok
 
 import com.anwang.safewallet.safekit.model.SafeInfo
+import com.anwang.safewallet.safekit.model.SafeUsdtInfo
 import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -16,8 +17,15 @@ class SafeProvider(baseUrl: String) {
         return service.getSafeInfo(netType)
     }
 
+    fun getSafeUsdtInfo(netType: String): Single<SafeUsdtInfo> {
+        return service.getSafeUsdtInfo(netType)
+    }
+
     private interface SafeService {
         @GET("v1/gate/{netType}")
         fun getSafeInfo(@Path("netType")netType: String): Single<SafeInfo>
+
+        @GET("v1/gate/{netType}")
+        fun getSafeUsdtInfo(@Path("netType")netType: String): Single<SafeUsdtInfo>
     }
 }
